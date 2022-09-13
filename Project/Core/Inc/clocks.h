4cd6996571ae29d669a4d9fdb0a9c5abced752d0
Matt Lemcke <lemckematt@gmail.com>
2022-09-13 19:14:48 -0400

    Added system clock configuration

---
+++ Project/Core/Inc/clocks.h
@@ -0,0 +1,15 @@
+/*
+ * clocks.h
+ *
+ *  Created on: Sep 8, 2022
+ *      Author: lemck
+ */
+
+#ifndef INC_CLOCKS_H_
+#define INC_CLOCKS_H_
+
+#include "stm32f429xx.h"
+
+void SysClockConfig(void);
+
+#endif /* INC_CLOCKS_H_ */

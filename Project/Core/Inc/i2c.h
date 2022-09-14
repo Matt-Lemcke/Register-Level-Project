01364f8d74e80c2d5bd568f32c82caa867034f3b
Matt Lemcke <53319315+Matt-Lemcke@users.noreply.github.com>
2022-09-14 14:33:35 -0400

    Comments to fix struct to minimize padding

--- Project/Core/Inc/i2c.h
+++ Project/Core/Inc/i2c.h
@@ -22,13 +22,14 @@ typedef enum {
 	I2C_2 = 1,
 	I2C_3 = 2,
 	I2C_Port_Count = 3
-}I2C_Port_e;
+}I2C_Port_e; //Should be 1 byte sized
 
+// Change later to adjust for padding
 typedef struct {
-	I2C_Port_e port;
-	uint16_t speed;
+	I2C_Port_e port; //1 byte?
+	uint16_t speed; //2 bytes
 
-	I2C_TypeDef *I2C;
+	I2C_TypeDef *I2C; //4 bytes?
 
 }I2C_Handler_t;
 

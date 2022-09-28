c7af5e94587ad13a93737b3afbcdb5f5b3ec8fcb
Matt Lemcke <53319315+Matt-Lemcke@users.noreply.github.com>
2022-09-28 10:17:00 -0400

    Added i2c.h comments

--- Project/Core/Inc/i2c.h
+++ Project/Core/Inc/i2c.h
@@ -12,9 +12,13 @@
 #include "stm32f429xx.h"
 #include "gpio.h"
 
+// I2C read/write buffer size
 #define BUFFER_SIZE 32
+
+// Max number of attempts to connect to device
 #define MAX_ATTEMPTS 20
 
+// Optionn to send stop after write function
 #define NO_STOP	0
 #define STOP 	1
 
@@ -27,20 +31,19 @@ typedef enum {
 	I2C_NACK
 }I2C_Status_e;
 
-// Change later to adjust for padding
+// Group into 4 byte sections to avoid padding
 typedef struct {
-	uint16_t speed; // In kHz // 2 bytes
+	uint16_t speed;		// In kHz
 	uint8_t scl_pin : 4;
 	uint8_t sda_pin	: 4;
 
-	I2C_TypeDef *I2C; // 4 bytes
-
-	uint8_t buffer[BUFFER_SIZE + 2];	// 34 bytes
-
+	I2C_TypeDef *I2C; 
+	
 	GPIO_Handle_t *gpio;
 
+	uint8_t buffer[BUFFER_SIZE + 2];
+	
 }I2C_Handler_t;
-
 
 I2C_Status_e I2C_Init(I2C_Handler_t *handler);
 I2C_Status_e I2C_Test_Device(I2C_Handler_t *handler, uint8_t slave_addr);
